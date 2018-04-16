6b47cad4ef2361cc4d9bc26f5d20b5ad4df75ab6
Gerson Neto <neto_mvn@hotmail.com>
2018-04-16 15:28:56 -0300

    criado método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,5 +1,14 @@
 class Pessoa:  # criar seus types personalizados
-    pass
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
 
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
 
+##Obs.: método nada mais é que uma função que pertence a uma classe
+# portanto, sem estar conectada a um objeto
 
+#
