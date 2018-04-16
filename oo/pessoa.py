198401efc40c824feeb6cbb54acd671709fba8ef
Gerson Neto <neto_mvn@hotmail.com>
2018-04-16 15:46:59 -0300

    criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,13 +1,21 @@
 class Pessoa:  # criar seus types personalizados
+    def __init__(self, nome = None, idade = 35):  # atributo do objeto
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luciano')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar()) # executando passando o próprio obj
+    print(p.nome)
+    p.nome = 'Renzo'
+    print(p.nome)
+    print(p.idade)
+
 
 ##Obs.: método nada mais é que uma função que pertence a uma classe
 # portanto, sem estar conectada a um objeto
-
