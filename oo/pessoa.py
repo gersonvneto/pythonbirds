12e5916bc3023de76caa150a38394ce0d0aa485b
Gerson Neto <neto_mvn@hotmail.com>
2018-04-18 19:16:48 -0300

    criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,21 +1,22 @@
 class Pessoa:  # criar seus types personalizados
-    def __init__(self, nome = None, idade = 35):  # atributos de dados
+    def __init__(self, *filhos, nome = None, idade = 35):  # atributos de dados
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Luciano')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar()) # executando passando o próprio obj
-    print(p.nome)
-    p.nome = 'Renzo'
-    print(p.nome)
-    print(p.idade)
-
+    renzo = Pessoa(nome='Renzo')
+    luciano = Pessoa(renzo, nome = 'Luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar()) # executando passando o próprio obj
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
 
 ##Obs.: método nada mais é que uma função que pertence a uma classe
 # portanto, sempre está conectada a um objeto
